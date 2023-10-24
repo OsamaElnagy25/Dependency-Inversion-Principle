e61d00b29a96899c4e154b7320a790cbc02768fc
Osama El Nagy <oelnagy@turndigital.net>
2023-10-24 16:11:57 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        var employeeDetailsModified = new EmployeeDetailsModified(new SalaryCalculatorModified());
+        employeeDetailsModified.HourlyRate = 50;
+        employeeDetailsModified.HoursWorked = 150;
+        Console.WriteLine($"The Total Pay is {employeeDetailsModified.GetSalary()}");
+    }
+}
+// Following the Dependency Inversion Principle
+public interface ISalaryCalculator
+{
+    float CalculateSalary(int hoursWorked, float hourlyRate);
+}
+public class SalaryCalculatorModified : ISalaryCalculator
+{
+    public float CalculateSalary(int hoursWorked, float hourlyRate) => hoursWorked * hourlyRate;
+}
+public class EmployeeDetailsModified
+{
+    private readonly ISalaryCalculator _salaryCalculator;
+
+    public int HoursWorked { get; set; }
+    public int HourlyRate { get; set; }
+    public EmployeeDetailsModified(ISalaryCalculator salaryCalculator)
+    {
+        _salaryCalculator = salaryCalculator;
+    }
+    public float GetSalary()
+    {
+        return _salaryCalculator.CalculateSalary(HoursWorked, HourlyRate);
+    }
+}
+
